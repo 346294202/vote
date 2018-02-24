ed168166541b59c7fe800e426de4244f55f4845d
Wang Jiang <sillyemperor@163.com>
2018-02-25 00:10:59 +0800

    重构，准备APP API开发

--- vote-api/src/main/java/com/leoyon/vote/api/GeneralWebConfig.java
+++ vote-api/src/main/java/com/leoyon/vote/api/GeneralWebConfig.java
@@ -1,6 +1,12 @@
 package com.leoyon.vote.api;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,4 +26,19 @@ public class GeneralWebConfig extends WebMvcConfigurerAdapter {
 		super.addInterceptors(registry);
 	}
 	
+	@Autowired
+	private ApplicationContext context;
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		
+		Map<String, HandlerMethodArgumentResolver> map = context.getBeansOfType(HandlerMethodArgumentResolver.class);
+		
+		for(HandlerMethodArgumentResolver i:map.values()) {
+			if(i.getClass().getPackage().getName().startsWith("com.leoyon.vote"))
+				argumentResolvers.add(i);
+		}
+		
+		super.addArgumentResolvers(argumentResolvers);
+	}
 }
