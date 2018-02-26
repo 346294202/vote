9cdeed002f6627c38088dc5d08bcd2c93e15f32b
chenyongfan <417744798@qq.com>
2018-02-26 10:18:36 +0800

    数据字典

---
+++ vote-admin-api/src/test/java/test/com/leoyon/vote/SysDictionaryControllerTests.java
@@ -0,0 +1,71 @@
+package test.com.leoyon.vote;
+
+import com.leoyon.vote.api.JsonResponse;
+import com.leoyon.vote.dictionary.SysDictionary;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.*;
+
+/**
+ * Created by Thinkpad on 2018/2/24.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {Application4Test.class},webEnvironment = SpringBootTest.WebEnvironment
+        .RANDOM_PORT)
+public class SysDictionaryControllerTests extends BaseWebTests {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        setToken(defUID);
+        //dbUtil.clear("sys_dictionary");
+    }
+
+
+
+    @Test
+    public void add() throws Exception {
+        SysDictionary p=new SysDictionary();
+        p.setSo(1);
+        p.setDictionaryName("123");
+        p.setRemark("321");
+        p.setCategoryName(1);
+        for (int i=0;i<10;i++){
+            JsonResponse r = restTemplate.postForObject("/sys/dictionary/add",p, JsonResponse.class);
+            assertSucess(r);
+        }
+
+
+    }
+
+    @Test
+    public void find() throws Exception {
+        JsonResponse r = restTemplate.getForObject("/sys/dictionary/find", JsonResponse.class);
+        assertSucess(r);
+        List<Map<String, Object>> items = (List<Map<String, Object>>) r.getItem("items");
+        System.out.println(items.toString());
+    }
+
+    @Test
+    public void update() throws Exception {
+        SysDictionary p=new SysDictionary();
+        p.setId(35L);
+        p.setSo(1);
+        p.setDictionaryName("111");
+        p.setRemark("111");
+        p.setCategoryName(1);
+        JsonResponse r = restTemplate.postForObject("/sys/dictionary/update/35",p, JsonResponse.class);
+        assertSucess(r);
+    }
+
+    @Test
+    public void delete() throws Exception {
+       restTemplate.delete("/sys/dictionary/delete/35",35L);
+    }
+
+
+
+}
