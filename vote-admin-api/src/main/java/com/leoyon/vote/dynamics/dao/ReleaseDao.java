c4d06daba9f66e52644bc6a3a5fb0430a605d5fa
chenyongfan <417744798@qq.com>
2018-02-27 14:40:18 +0800

    数据字典

---
+++ vote-admin-api/src/main/java/com/leoyon/vote/dynamics/dao/ReleaseDao.java
@@ -0,0 +1,24 @@
+package com.leoyon.vote.dynamics.dao;
+
+import com.leoyon.vote.dictionary.FindSysDictionaryRequest;
+import com.leoyon.vote.dictionary.FindSysDictionaryResponse;
+import com.leoyon.vote.dictionary.SysDictionary;
+import com.leoyon.vote.dynamics.FindReleaseRequest;
+import com.leoyon.vote.dynamics.FindReleaseResponse;
+import com.leoyon.vote.dynamics.Release;
+
+import java.util.List;
+
+/**
+ * Created by Thinkpad on 2018/2/26.
+ */
+public interface ReleaseDao {
+
+    List<FindReleaseResponse> find(FindReleaseRequest rqst);
+
+    void add(Release release);
+
+    void update(Release release);
+
+    void delete(Release release);
+}
