9ed7d212d5a9998a2fafdcbc33eea70d32e08ce9
Wang Jiang <sillyemperor@163.com>
2018-03-05 16:13:18 +0800

    api doc

--- vote-app-api/src/main/java/com/leoyon/vote/advice/Advice.java
+++ vote-app-api/src/main/java/com/leoyon/vote/advice/Advice.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import com.leoyon.vote.picture.Picture;
 
+import wj.flydoc.ApiIgnore;
 import wj.flydoc.ApiParam;
 
 public class Advice {
@@ -16,18 +17,24 @@ public class Advice {
 	private Integer status;
 	private Integer type;
 	private Collection<Picture> pictures;
+	
+	@ApiParam(desc="建议id，提交时忽略")
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	@ApiIgnore
 	public Long getUserId() {
 		return userId;
 	}
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
+	
+	
 	public String getPhone() {
 		return phone;
 	}
@@ -40,18 +47,23 @@ public class Advice {
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
+	@ApiParam(desc="日期，提交时忽略")
 	public Date getDateCreate() {
 		return dateCreate;
 	}
 	public void setDateCreate(Date dateCreate) {
 		this.dateCreate = dateCreate;
 	}
+	
+	@ApiParam(desc="状态，提交时忽略")
 	public Integer getStatus() {
 		return status;
 	}
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+	
 	@ApiParam(desc="1 投诉，2 建议")
 	public Integer getType() {
 		return type;
